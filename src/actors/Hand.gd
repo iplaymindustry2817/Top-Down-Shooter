a8123fdfbd223a6415aef1c124fc519fd3feb100
ScatterInk <57494910+iplaymindustry2817@users.noreply.github.com>
2020-04-23 22:08:27 -0700

    Added hand icon
    when you put the player grab range on a pickup a hand will apear.

--- src/actors/Hand.gd
+++ src/actors/Hand.gd
@@ -1,7 +1,24 @@
 extends Area2D
 
-var this: = self
+var is_colliding: = false
+var collider
+var collider_layer
 
 func _process(delta):
-	if this.is_colliding():
-		print("hand")
+	if is_colliding and collider_layer >= 2^(6 - 1):
+		$Hand.visible = true
+		$Sprite.visible = false
+		if Input.is_action_just_pressed("Interact"):
+			print("JUNK")
+	else:
+		$Hand.visible = false
+		$Sprite.visible = true
+
+func _on_Hand_body_entered(body: PhysicsBody2D):
+	is_colliding = true
+	collider = body
+	collider_layer = body.get_collision_layer()
+
+
+func _on_Hand_body_exited(body):
+	is_colliding = false
